b1424764ee064b0d475b9d8e5554fe69be69f862
anihsugiarti9 <137507644+anihsugiarti9@users.noreply.github.com>
2025-05-01 04:47:35 +0700

    Create dev.nix

---
+++ dev.nix
@@ -0,0 +1,47 @@
+# wolu
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  packages = [
+    pkgs.jdk17
+    pkgs.unzip
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    extensions = [
+      "Dart-Code.flutter"
+      "Dart-Code.dart-code"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        build-flutter = ''
+          cd /home/user/myapp/android
+
+          ./gradlew \
+            --parallel \
+            -Pverbose=true \
+            -Pdart-defines=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC85NzU1MDkwN2I3MGY0ZjNiMzI4YjZjMTYwMGRmMjFmYWMxYTE4ODlhLw== \
+            -Pdart-obfuscation=false \
+            -Ptrack-widget-creation=true \
+            -Ptree-shake-icons=false \
+            -Pfilesystem-scheme=org-dartlang-root \
+            assembleDebug
+        '';
+      };
+      
+      # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
+          manager = "flutter";
+        };
+      };
+    };
+  };
+}
